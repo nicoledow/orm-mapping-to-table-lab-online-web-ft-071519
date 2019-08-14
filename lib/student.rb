7985704975f5f7f3015ac4add2161a73ecaf5f6c
Nicole Dow <nikki.dow71@gmail.com>
2019-08-14 00:29:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,12 @@ class Student
   end
   
   def self.create_table
-    
+    sql = <<- SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+        )
   end
   
 end
